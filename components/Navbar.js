27c1c3079c56c36c9838d7adbf4e08247ea85eae
Brian Ngechu <brian.ngechu@tracom.co.ke>
2021-07-20 19:15:15 +0300

    Add cart button to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,45 @@
-import Image from "next/image";
+import Link from "next/link";
+import { useContext, useEffect } from "react";
+import { CartContext } from "../pages/_app";
 
 const Navbar = () => {
+  let myCart = "";
+  if (typeof window !== "undefined") {
+    myCart = JSON.parse(localStorage.getItem("cart"));
+  }
+
+  const { cartCount, setCartCount } = useContext(CartContext);
+  useEffect(() => {
+    if (myCart.length) {
+      setCartCount(myCart.length);
+    }
+  }, [cartCount]);
+
   return (
     <nav className="sticky top-0 z-10">
       <div className="box-border h-16 w-full flex items-center bg-blue-900 px-2 ">
-        <svg
-          className="md:w-8 h-10 stroke-current text-gray-200 mx-auto"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-          />
-        </svg>
+        <div className="container w-3/4 mx-auto">
+          <Link href="/">
+            <a>
+              <div className="inline-block font-semibold text-2xl text-white">
+                Home
+              </div>
+            </a>
+          </Link>
+          <div className="inline-block float-right">
+            <Link href="/cart">
+              <a
+                className="px-4 py-2 mt-2 text-lg text-gray-900 bg-gray-200 rounded-lg sm:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-300 "
+                href="#"
+              >
+                <span className="badge mb-3 bg-red-800 rounded-full px-2 py-1 text-center object-left-top text-white text-sm mr-1">
+                  {cartCount}
+                </span>
+                My Cart
+              </a>
+            </Link>
+          </div>
+        </div>
       </div>
     </nav>
   );
