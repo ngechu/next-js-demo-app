5250d14bab42d67c0a5ab81c9d4a10f7a1d7c32b
Brian Ngechu <brian.ngechu@tracom.co.ke>
2021-07-20 19:18:29 +0300

    Make async calls to fetch products

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,52 @@
 import Image from "next/image";
 import Card from "../components/Card";
 import Hero from "../components/Hero";
-import Link from "next/link";
-import { Skeleton } from "antd";
-export default function Home() {
+import { Skeleton, Space, Divider } from "antd";
+import { useQuery, QueryClient } from "react-query";
+import { dehydrate } from "react-query/hydration";
+
+//Fetch data
+const fetchProducts = async () => {
+  const response = await fetch("https://fakestoreapi.com/products?limit=8");
+  const data = await response.json();
+  return data;
+};
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery("products", fetchProducts);
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
+
+export default function Home({ products }) {
+  // Access the client
+  const { isLoading, data, error } = useQuery("products", fetchProducts);
+  if (isLoading) {
+    return <Skeleton active size="large" />;
+  }
+
   return (
     <div className="flex flex-col">
       <div className="">
         <Hero />
       </div>
-      <div className="h-180 p-6 flex flex-row flex-wrap justify-around  content-center gap-6 box-border">
-        <Link href="#">
-          <a>
-            <Card title="Iphone X" category="Built by passion" price={2000} />
-          </a>
-        </Link>
-        <Link href="#">
-          <a>
-            <Card title="Iphone X" category="Built by passion" price={2000} />
-          </a>
-        </Link>
-        <Link href="#">
-          <a>
-            <Card title="Iphone X" category="Built by passion" price={2000} />
-          </a>
-        </Link>
-        <Link href="#">
-          <a>
-            <Card title="Iphone X" category="Built by passion" price={2000} />
-          </a>
-        </Link>
-        <Link href="#">
-          <a>
-            <Card title="Iphone X" category="Built by passion" price={2000} />
-          </a>
-        </Link>
-        <Link href="#">
-          <a>
-            <Card title="Iphone X" category="Built by passion" price={2000} />
-          </a>
-        </Link>
+      <div className="h-180 p-6 flex flex-row flex-wrap justify-start content-center gap-6">
+        {data.map((product) => {
+          return (
+            <Card
+              title={product.title}
+              category={product.category}
+              price={product.price}
+              image={product.image}
+              id={product.id}
+              key={product.id}
+            />
+          );
+        })}
       </div>
       <div className="h-80">
         <Image
