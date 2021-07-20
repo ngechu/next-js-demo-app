27ca530d2b1dac12d037cf1e13fd59f7be8e8e43
Brian Ngechu <brian.ngechu@tracom.co.ke>
2021-07-20 19:10:47 +0300

    Add to cart fuctionality

--- components/Card.js
+++ components/Card.js
@@ -1,24 +1,58 @@
-import Image from "next/image";
+import Link from "next/link";
+import { useContext, useEffect } from "react";
+import { CartContext } from "../pages/_app";
 
-const Card = ({ title, category, price }) => {
+const Card = ({ title, category, price, image, id }) => {
+  const { cartCount, setCartCount } = useContext(CartContext);
+
+  let cart;
+  if (typeof window !== "undefined") {
+    cart = JSON.parse(`${localStorage.getItem("cart") || "[]"}`);
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
+  const handleClick = () => {
+    let productObj = {
+      title: title,
+      category: category,
+      price: price,
+      image: image,
+      id: id,
+    };
+    cart.push(productObj);
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    setCartCount(cart.length);
+  };
+
   return (
-    <div class="w-full md:w-1/2 lg:w-80 pl-5 pr-5 mb-5 lg:pl-2 lg:pr-2">
-      <div class="bg-white rounded-lg m-h-64 p-2 transform hover:translate-y-2 hover:shadow-xl transition duration-300">
-        <figure class="mb-2">
-          <img
-            src="https://srv-cdn.onedio.com/store/988bccbdb9ca395f581f98faa9ce3a55123f12bfcef608c838532b813646e557.png"
-            alt=""
-            class="h-64 ml-auto mr-auto"
-          />
+    <div className="w-full md:w-80 lg:w-80 pl-2 pr-2 mb-5 lg:pl-2 lg:pr-2">
+      <div className="bg-white rounded-lg m-h-64 p-2 transform hover:translate-y-2 hover:shadow-xl transition duration-300">
+        <figure className="mb-2">
+          <Link href={`/products/${id}`}>
+            <a>
+              <img src={image} alt="" className="h-64 ml-auto mr-auto" />
+            </a>
+          </Link>
         </figure>
-        <div class="rounded-lg p-4 bg-gray-700 flex flex-col text-center">
+        <div className="rounded-lg p-4 bg-gray-700 flex flex-col text-center">
           <div>
-            <h5 class="text-white text-2xl font-bold leading-none">{title}</h5>
-            <span class="text-xs text-gray-400 leading-none">{category}</span>
+            <h5 className="text-white text-xl font-semibold leading-none truncate">
+              {title}
+            </h5>
+            <span className="text-xs text-gray-400 leading-none">
+              {category}
+            </span>
           </div>
-          <div class="flex justify-center">
-            <h2 class="text-lg text-white font-light">${price}</h2>
+          <div className="flex justify-center">
+            <h2 className="text-lg text-white font-semibold">$ {price}</h2>
           </div>
+          <button
+            onClick={handleClick}
+            className="bg-gray-600 inline-block w-auto h-10 text-white hover:bg-blue-700"
+          >
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
